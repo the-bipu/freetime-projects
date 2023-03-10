64eb1f7ad3bfe0a574a0b0a49b217666570324fc
The Bipu <127449591+the-bipu@users.noreply.github.com>
2023-03-11 00:37:10 +0530

    Create more sets
    Created more sets of questions and linked them with the main page.

---
+++ Set-OA/Set-OAB.js
@@ -0,0 +1,47 @@
+// creating an array and passing the number , questions , options and answers
+let questions = [
+    {
+        numb: 1,
+        question: "SMALLEST LINE SPACING IN MS WORD IS ______?",
+        answer: "1.0",
+        options: [
+            "1.5",
+            "1.0",
+            "2.0",
+            "1.1"
+        ]
+    },
+    {
+        numb: 2,
+        question: "HOW MANY DEFAULT COLUMNS OPTIONS  IN MS WORD?",
+        answer: "NONE OF THE ABOVE",
+        options: [
+            "2",
+            "4",
+            "6",
+            "NONE OF THE ABOVE"
+        ]
+    },
+    {
+        numb: 3,
+        question: "SHOTCUT KEY FOR PAGE BREAKING IS __________ ?",
+        answer: "CTRL+ENTER",
+        options: [
+            "CTRL+SHIFT",
+            "CTRL+BACKSPACE",
+            "CTRL+TAB",
+            "CTRL+ENTER"
+        ]
+    },
+    {
+        numb: 4,
+        question: "WHAT IS THE DEFAULT SIZE OF THE PAGE?",
+        answer: "LETTER",
+        options: [
+            "LEGAL",
+            "A4",
+            "A3",
+            "LETTER"
+        ]
+    },
+];
