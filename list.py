b412184ebc3fdc3b8fd917cd7069bdfe592a65fb
The Bipu <127449591+the-bipu@users.noreply.github.com>
2023-04-16 13:38:43 +0530

    files updation
    updated some files and added some new projects in the list.

---
+++ list.py
@@ -0,0 +1,27 @@
+# Band name generator 
+
+# Tip Calculator
+
+# love calculator
+
+# Tresure Island
+
+# rock paper scissor
+
+# random coins
+
+# password generator
+
+# Hangman
+
+# Caesar Cipher
+
+# Secret Auction
+
+# Calculator
+
+# BlackJack Game
+
+# Number guessing 
+
+# Higher Lower
