af0bc0174b0b3f7ab3705b5d37bd6d448aaac479
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-01-03 23:34:38 +0530

    create index page

---
+++ Task-Dashboard/frontend/src/index/View/IndexView.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndexView = () => {
+    return (
+        <div className='flex justify-center pt-10'>
+           Welcome to the Task Manager
+        </div>
+    );
+};
+
+export default IndexView;
