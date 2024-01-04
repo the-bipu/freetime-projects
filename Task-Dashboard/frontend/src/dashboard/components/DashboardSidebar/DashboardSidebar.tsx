5c8cbc1f390ec5f3ef2123bac831faecf50b1c6f
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-01-04 23:46:22 +0530

    Update DashboardSidebar.tsx

--- Task-Dashboard/frontend/src/dashboard/components/DashboardSidebar/DashboardSidebar.tsx
+++ Task-Dashboard/frontend/src/dashboard/components/DashboardSidebar/DashboardSidebar.tsx
@@ -8,28 +8,32 @@ interface DashboardSidebarProps {
 
 const DashboardSidebar: React.FC<DashboardSidebarProps> = ({ showdb, clickedButton, handleButtonClick }) => {
     return (
-        <div className={`flex flex-row items-center justify-between 'w-full' h-20 bg-[#0d0c22] px-6`}>
-            <div className='flex flex-row justify-center text-white text-base font-bold text-center gap-6'>
+        <div className={`flex flex-row items-center justify-end 'w-full' h-20 bg-[#ffffff] px-12`}>
+            <div className='flex flex-row justify-center text-black text-base font-bold text-center gap-2'>
                 <button
-                    className={`py-2 px-6 rounded-lg ${clickedButton === 'All' ? 'bg-[#2f2f2f]' : ''}`}
+                    id={`${clickedButton === 'All' ? 'bgBlue' : ''}`}
+                    className={`py-2 px-6 rounded-lg ${clickedButton === 'All' ? 'shadowCard' : ''}`}
                     onClick={() => handleButtonClick('All')}
                 >
                     All
                 </button>
                 <button
-                    className={`py-2 px-6 rounded-lg ${clickedButton === 'Upcoming' ? 'bg-[#2f2f2f]' : ''}`}
+                    id={`${clickedButton === 'Upcoming' ? 'bgBlue' : ''}`}
+                    className={`py-2 px-6 rounded-lg ${clickedButton === 'Upcoming' ? 'shadowCard' : ''}`}
                     onClick={() => handleButtonClick('Upcoming')}
                 >
                     Upcoming
                 </button>
                 <button
-                    className={`py-2 px-6 rounded-lg ${clickedButton === 'Overdue' ? 'bg-[#2f2f2f]' : ''}`}
+                    id={`${clickedButton === 'Overdue' ? 'bgBlue' : ''}`}
+                    className={`py-2 px-6 rounded-lg ${clickedButton === 'Overdue' ? 'shadowCard' : ''}`}
                     onClick={() => handleButtonClick('Overdue')}
                 >
                     Overdue
                 </button>
                 <button
-                    className={`py-2 px-6 rounded-lg ${clickedButton === 'Completed' ? 'bg-[#2f2f2f]' : ''}`}
+                    id={`${clickedButton === 'Completed' ? 'bgBlue' : ''}`}
+                    className={`py-2 px-6 rounded-lg ${clickedButton === 'Completed' ? 'shadowCard' : ''}`}
                     onClick={() => handleButtonClick('Completed')}
                 >
                     Completed
