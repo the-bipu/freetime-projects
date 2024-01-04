6979d1a95948e9ae2a36855630c35ac7e5550697
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-01-05 00:28:48 +0530

    Create Navbar.tsx

---
+++ Task-Dashboard/frontend/src/dashboard/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full bgLight flex flex-row justify-between px-24 py-2'>
+        <div className='px-1 py-3 text-xl font-bold'>hello</div>
+        <div className='px-1 py-3 text-xl font-bold'>Hello</div>
+    </div>
+  )
+}
+
+export default Navbar
