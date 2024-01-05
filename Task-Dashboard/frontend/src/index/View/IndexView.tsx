b4f3f98ac4543f28d031f3a627b5c2b66dc1321d
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-01-05 15:01:03 +0530

    Update IndexView.tsx

--- Task-Dashboard/frontend/src/index/View/IndexView.tsx
+++ Task-Dashboard/frontend/src/index/View/IndexView.tsx
@@ -1,12 +1,22 @@
+'use client';
+
 import React from 'react';
+import { useRouter } from 'next/router';
 
 const IndexView = () => {
+
+  const router = useRouter();
+
+  const toggleDB = () => {
+    router.push('/Dashboard');
+  };
+
     return (
         <div className='flex flex-col justify-center items-center w-full pt-10'>
            Welcome to the Task Manager
 
            <div className='mt-8'>
-             <a href="/Dashboard">Go to Dashboard</a>
+             <button onClick={toggleDB}>Go to Dashboard</button>
            </div>
         </div>
     );
