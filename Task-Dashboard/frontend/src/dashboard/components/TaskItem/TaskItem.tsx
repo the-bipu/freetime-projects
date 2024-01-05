8f6d0a9cb0271218ee22e21e4ec9772ee311c1d9
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-01-05 15:01:08 +0530

    Update TaskItem.tsx

--- Task-Dashboard/frontend/src/dashboard/components/TaskItem/TaskItem.tsx
+++ Task-Dashboard/frontend/src/dashboard/components/TaskItem/TaskItem.tsx
@@ -24,14 +24,14 @@ const TaskItem: React.FC<TaskItemProps> = ({ task, index, currentDate, handleDel
             <div className='relative flex flex-col w-full gap-4'>
                 <div className='flex flex-row justify-between'>
                     <div
-                        className={`w-auto px-4 py-1 flex items-center justify-center rounded-md text-white font-bold text-sm 
+                        className={`w-auto px-4 py-1 flex items-center justify-center rounded-md text-white font-light text-sm 
                                 ${task.priority === 'Hard' ? 'bg-red-500' :
                                 task.priority === 'Medium' ? 'bg-orange-500' :
                                     'bg-green-500'}`}
                     >
                         {task.priority}
                     </div>
-                    <div className='w-auto bg-[#3638b8] px-4 py-1 flex items-center justify-center rounded-md text-white font-bold text-sm'>
+                    <div className='w-auto bg-[#3638b8] px-4 py-1 flex items-center justify-center rounded-md text-white font-light text-sm'>
                         {task.dueDate instanceof Date ? task.dueDate.toDateString() : ''}
                     </div>
                 </div>
@@ -46,13 +46,20 @@ const TaskItem: React.FC<TaskItemProps> = ({ task, index, currentDate, handleDel
                 <div className='flex flex-row justify-between'>
                     <div className='font-light text-gray-500 text-sm flex flex-row gap-2 items-center'>
                         <div>
-                            {task.dueDate.toDateString() === currentDate.toDateString() ? (
-                                <div>Today</div>
-                            ) : task.dueDate > currentDate ? (
-                                <div>Upcoming</div>
-                            ) : (
-                                <div>Gone</div>
+                            {task.dueDate instanceof Date && (
+                                <>
+                                    {
+                                        task.dueDate.toDateString() === currentDate.toDateString() ? (
+                                            <div>Today</div>
+                                        ) : task.dueDate > currentDate ? (
+                                            <div>Upcoming</div>
+                                        ) : (
+                                            <div>Gone</div>
+                                        )
+                                    }
+                                </>
                             )}
+
                         </div>
                         <div className='text-lg flex items-center justify-center'>
                             <button>
