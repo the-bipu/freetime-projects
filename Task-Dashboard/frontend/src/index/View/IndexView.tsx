f11d90b32ec5cf7c8c2247f76607fbb1c7e7121a
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-01-05 21:23:07 +0530

    update index page

--- Task-Dashboard/frontend/src/index/View/IndexView.tsx
+++ Task-Dashboard/frontend/src/index/View/IndexView.tsx
@@ -3,6 +3,9 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
+import { FaChartPie } from "react-icons/fa";
+import './indexView.scss';
+
 const IndexView = () => {
 
   const router = useRouter();
@@ -11,15 +14,86 @@ const IndexView = () => {
     router.push('/Dashboard');
   };
 
-    return (
-        <div className='flex flex-col justify-center items-center w-full pt-10'>
-           Welcome to the Task Manager
+  return (
+    <div id='bgLightNormal' className='w-full h-full flex flex-row'>
 
-           <div className='mt-8'>
-             <button onClick={toggleDB}>Go to Dashboard</button>
-           </div>
+      <div className='h-screen w-1/5 bg-[#F1F1FB] flex flex-col items-center py-4 border-r-2 border-zinc-300'>
+
+        <div className='py-10 flex flex-row items-center justify-center gap-2'>
+          <div className='w-16 h-16 bg-white rounded-lg'></div>
+          <div className='text-sm font-bold flex flex-col text-[#222A51]'>
+            <span>O2O</span>
+            <span>Task</span>
+            <span>Dashboard</span>
+          </div>
         </div>
-    );
+
+        <div className='w-full h-full flex flex-col items-center justify-start gap-6 pt-10'>
+
+          <div className='text-lg font-semibold py-2 flex flex-row items-center gap-2 cursor-pointer text-[#222A51]'>
+            <FaChartPie />
+            Summary
+          </div>
+
+          <button className='text-lg font-semibold py-2 flex flex-row items-center gap-2 cursor-pointer text-[#222A51]' onClick={toggleDB}>
+            <FaChartPie />
+            Tasks
+          </button>
+
+        </div>
+      </div>
+
+      <div className='h-screen w-3/5 bg-[#F1F1FB] flex flex-col pt-12 px-16'>
+
+        <div className='flex flex-col gap-12'>
+          <h3 className='text-2xl font-bold text-[#222A51]'>Tasks Dashboard</h3>
+
+          <div className='flex flex-row gap-10'>
+
+            <div className='w-60 h-20 bg-[#5A59E3] rounded-xl px-6 py-4 text-white'>
+              <p className='font-bold text-sm'>Total Task</p>
+              <p className='font-bold text-xs'>7</p>
+            </div>
+            <div className='w-60 h-20 bg-[#5A59E3] rounded-xl px-6 py-4 text-white'>
+              <p className='font-bold text-sm'>Pending Task</p>
+              <p className='font-bold text-xs'>4</p>
+            </div>
+            <div className='w-60 h-20 bg-[#5A59E3] rounded-xl px-6 py-4 text-white'>
+              <p className='font-bold text-sm'>Completed Task</p>
+              <p className='font-bold text-xs'>3</p>
+            </div>
+
+          </div>
+
+        </div>
+
+        <div className='flex flex-col gap-6 pt-10'>
+          <h3 className='text-lg font-bold text-[#222A51]'>Marketplace</h3>
+
+          <div className='flex flex-row gap-8'>
+
+            <div className='w-3/5 h-full flex flex-col gap-10'>
+              <div className='w-full h-40 bg-[#fff] rounded-xl px-6 py-4'>
+                <p className='font-bold text-base'>Total Task</p>
+                <p className='font-bold text-base'>7</p>
+              </div>
+              <div className='w-full h-40 bg-[#fff] rounded-xl px-6 py-4'>
+                <p className='font-bold text-base'>Pending Task</p>
+                <p className='font-bold text-base'>4</p>
+              </div>
+            </div>
+            <div className='w-2/5 h-auto bg-[#fff] rounded-xl'></div>
+
+          </div>
+        </div>
+      </div>
+
+      <div className='h-screen w-1/5 bg-[#fff] flex flex-col pt-12 px-16'>
+        <button onClick={toggleDB}>Tasks</button>
+      </div>
+
+    </div>
+  );
 };
 
 export default IndexView;
